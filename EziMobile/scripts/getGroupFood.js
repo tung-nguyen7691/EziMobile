207f9afe56ee6796adebe533f5a3b9f14ff4277d
Minh Nguyen <truyennhanvk08@gmail.com>
2014-05-30 21:28:33 -0500

    Changes made by Minh Nguyen (truyennhanvk08@gmail.com).

--- EziMobile/scripts/getGroupFood.js
+++ EziMobile/scripts/getGroupFood.js
@@ -1,8 +1,36 @@
-function openModalViewGuestQuantity() {
-    $("#modalguestquantity").kendoMobileModalView("open");
+function getItemGroup() {
+    REAID = 1;
+    CurrencyID = 1;
+    levelgroup= 1;
+    _branchID = 3;
+    _langID = 2;
     
-}
-function closeModalViewGuestQuantity() {
-    $("#modalguestquantity").kendoMobileModalView("close");
-    
+    var dataRequest = {
+        branchID: _branchID,
+        levelgroup: levelgroup,
+        langID: _langID,
+        REAID: REAID,
+        CurrencyID: CurrencyID
+    };
+	$.ajax({
+               url: _webServicePath + "getAllItemGroup",
+               type: "POST",
+               dataType: "json",
+               data: dataRequest,
+               contentType: "application/json; charset=utf-8",
+               failure: function() {
+                   alert("Load Data Failed");
+                   return null;
+               },
+               success: function(data) {
+                   var result = JSON.parse(data.d.Result);
+                   if (result.ITEMGROUP !== null) {
+                       return result.ITEMGROUP;
+                   } else{
+                       alert("Load Data Failed");
+                       return null;
+                       }
+                   
+               }
+           })
 }
