b9a76e6fccfaa2445e7530a47d3f7190542574dc
Duc Nguyen <ducnt@vnyi.com>
2014-05-31 00:01:39 -0500

    Changes made by Duc Nguyen (ducnt@vnyi.com).

--- EziMobile/scripts/dineIn.js
+++ EziMobile/scripts/dineIn.js
@@ -5,9 +5,9 @@ function listViewAreaBind() {
     
     var dataRequest = {
         branchID: _branchID,
-        RectID: 1,
+        rectID: 1,
         langID: _langID,
-        UserID: _userID
+        userID: _userID
     };
     $.ajax({
                url: _webServicePath + 'getArea',
@@ -24,10 +24,50 @@ function listViewAreaBind() {
                        $("#dropdownArea").kendoDropDownList({
                                                                 dataTextField: "REA_NAME",
                                                                 dataValueField: "REA_AUTOID",
-                                                                dataSource: result.RESAREA
+                                                                dataSource: result.RESAREA,
+                                                                select: loadTableGrid
                                                             });
                    else
                        alert("No Areas Found");
+               }
+           })
+}
+
+function loadTableGrid(e) {
+    //selected item
+    var selectedItem = this.dataItem(e.item.index());
+    var reaID = selectedItem.REA_AUTOID;
+    _langID = 2;
+    _branchID = 3;
+    _userID = 1;
+
+    var dataRequest = {
+        reaID: reaID,
+        branchID: _branchID,
+        langID: _langID,
+        userID: _userID
+    };
+    $.ajax({
+               url: _webServicePath + 'getOpenTable',
+               type: "POST",
+               dataType: "json",
+               data: JSON.stringify(dataRequest),
+               contentType: "application/json; charset=utf-8",
+               failure: function() {
+                   alert("No Tables Found");
+               },
+               success: function(data) {
+                   var result = JSON.parse(data.d.Result);
+                   if (result.CustomerTable !== null) {
+                       var template = kendo.template("<svg style='padding: 5px' width='110' height='110'>" +
+                                                     "<rect width='110' height='110' rx='20' ry='20' stroke='cyan' stroke-width='3' fill='white' >" +
+                                                     "</rect>" +
+                                                     "<text x='20' y='15' fill='cyan'>#= RET_DEFINEID #</text>" +
+                                                     "</svg>");
+                       var displayData = kendo.render(template, result.CustomerTable); //render the template
+                       $("#tableGrid").html(displayData); //display the result
+                   } else
+                       alert("No Tables Found");
                }
            })
 }
