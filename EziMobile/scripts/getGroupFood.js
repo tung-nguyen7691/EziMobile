bbcbe7f5adf282c7a26828290bdb9d4cc21013e3
Minh Nguyen <truyennhanvk08@gmail.com>
2014-05-30 22:53:47 -0500

    Changes made by Minh Nguyen (truyennhanvk08@gmail.com).

--- EziMobile/scripts/getGroupFood.js
+++ EziMobile/scripts/getGroupFood.js
@@ -1,36 +1,35 @@
-function getItemGroup() {
-    REAID = 1;
-    CurrencyID = 1;
-    levelgroup= 1;
+var getGroupFood;
+                    
+$(function() {
+    reaID = '1';
+    currencyID = '1';
+    levelgroup = 1;
     _branchID = 3;
     _langID = 2;
     
     var dataRequest = {
-        branchID: _branchID,
+        reaID: reaID,
+        currencyID: currencyID,
         levelgroup: levelgroup,
-        langID: _langID,
-        REAID: REAID,
-        CurrencyID: CurrencyID
+        branchID: _branchID,
+        langID: _langID
     };
-	$.ajax({
+    $.ajax({
                url: _webServicePath + "getAllItemGroup",
                type: "POST",
                dataType: "json",
-               data: dataRequest,
+               data : JSON.stringify(dataRequest),
                contentType: "application/json; charset=utf-8",
                failure: function() {
                    alert("Load Data Failed");
-                   return null;
                },
                success: function(data) {
                    var result = JSON.parse(data.d.Result);
                    if (result.ITEMGROUP !== null) {
-                       return result.ITEMGROUP;
-                   } else{
+                       products = result.ITEMGROUP;
+                   } else {
                        alert("Load Data Failed");
-                       return null;
-                       }
-                   
+                   }
                }
            })
-}
+})
