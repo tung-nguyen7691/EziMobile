dfbb27e62fcd7144988adf5970de0c5d7f5efd09
Minh Nguyen <truyennhanvk08@gmail.com>
2014-06-01 20:42:47 -0500

    Changes made by Minh Nguyen (truyennhanvk08@gmail.com).

--- EziMobile/scripts/tableInformation.js
+++ EziMobile/scripts/tableInformation.js
@@ -1,8 +1,27 @@
 function openModalViewGuestQuantity() {
     $("#modalguestquantity").kendoMobileModalView("open");
-    
 }
 function closeModalViewGuestQuantity() {
     $("#modalguestquantity").kendoMobileModalView("close");
-    
+}
+function tableInformationLoad() {
+    $("#edtmale").kendoNumericTextBox({
+                                          spin: onSpin
+                                      });
+    $("#edtfemale").kendoNumericTextBox({
+                                          spin: onSpin
+                                      });
+    $("#edtchild").kendoNumericTextBox({
+                                          spin: onSpin
+                                      });
+    $("#inputGuestQuantity").kendoNumericTextBox();
+}
+
+function onSpin() {
+    var male = parseInt($('#edtmale').val());
+    var female = parseInt($('#edtfemale').val());
+    var child = parseInt($("#edtchild").val());
+    var total = male + female + child;
+    var txtGuestQuantity = $("#inputGuestQuantity").data("kendoNumericTextBox");
+    txtGuestQuantity.value(total);
 }
